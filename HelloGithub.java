2210c9bd6c43a05771a72b641dc9d9963214aa39
syedali-qa <syedalitest1@gmail.com>
2025-05-27 09:43:58 +0530

    adding new java file

---
+++ HelloGithub.java
@@ -0,0 +1,11 @@
+public class HelloGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub!");
+        greet("Syed");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Welcome, " + name + "!");
+    }
+}
